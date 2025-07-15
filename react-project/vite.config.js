0357575d6445c0a0851807c091e064fb9c499a57
Grant Coburn <140763612+coburnGrant@users.noreply.github.com>
2025-07-14 20:15:06 -0400

    config for render

--- react-project/vite.config.js
+++ react-project/vite.config.js
@@ -5,6 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: true
+    allowedHosts: true,
+    host: '0.0.0.0',
+    port: 3000
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 3000
   }
 })
